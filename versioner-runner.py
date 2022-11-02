8d02eef11cb359faf1f124e13e4202250a53ecf0
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2022-11-02 15:19:05 +0100

    chore(github-actions): change old set-output to env var output

--- versioner-runner.py
+++ versioner-runner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 from versioner.versioner import get_java_version
 
@@ -7,7 +8,10 @@ if __name__ == '__main__':
   version = get_java_version()
   # Output different if we are in github actions.
   if len(sys.argv) == 2 and sys.argv[1] == "ga":
-    print(f"::set-output name=java-version::{version}")
+    # print(f"::set-output name=java-version::{version}")
+    name = "java-version"
+    with open(os.getenv('GITHUB_OUTPUT'), 'a') as env:
+      print(f'{name}={version}', file=env)
   elif 'help' in sys.argv:
     print("usage: ./versioner-runner.py ga")
     print("Output the least Java version supported for the current Gradle wrapper.")
